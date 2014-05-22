991d860a05f56abace320af62e18244ee5e91658
xtoff <cerisara@loria.fr>
2014-05-22 17:20:24 +0200

    much more stable risk but very heuristic - TODO should be improved by iterating

--- src/jsafran/parsing/unsupLinear/ConstrainedLinearModel.java
+++ src/jsafran/parsing/unsupLinear/ConstrainedLinearModel.java
@@ -27,9 +27,16 @@ public class ConstrainedLinearModel {
     }
     public void randomInit() {
         Random random = new Random();
+        float s=0;
         for (int i = 0; i < nfts; i++) {
             // precision should be at least 1e-4
             w[i] = (float)(Math.round(random.nextDouble() * 100000.0) / 10000.0);
+            s+=w[i];
+        }
+        // forces the sum of weights to be 0
+        float m=s/(float)nfts;
+        for (int i = 0; i < nfts; i++) {
+            w[i]-=m;
         }
     }
     public float getNegError(ActionCorpus data) {
@@ -46,7 +53,7 @@ public class ConstrainedLinearModel {
                 // no more active features
                 break;
             }
-            scorePerLab[0] += w[i];
+            scorePerLab[0] += w[feats[i]];
         }
         scorePerLab[1]=-scorePerLab[0];
         int bestlab = scorePerLab[0]>=0?0:1;
@@ -58,7 +65,7 @@ public class ConstrainedLinearModel {
         return s;
     }
     public void updateWeight(int feat, int lab, float epsilon) {
-        float delta = lab==0?epsilon:-epsilon;
+        float delta = lab==0?epsilon/2f:-epsilon/2f;
         w[feat]+=delta;
     }
 }
